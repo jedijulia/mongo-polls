beefb0634956a8cef97ecac71fefc5190ba129cf
Julia Menchavez <jedijulia@users.noreply.github.com>
2015-10-01 22:01:00 +0800

    updated mysite urls to follow pep8

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
